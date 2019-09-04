9e98bceed531e1ccb56baff13c890e45dc0137e4
Breno Guilherme de Souza Lopes <46333794+brenodev@users.noreply.github.com>
2019-09-04 15:06:39 -0300

    Update webpack.config.js
    Atualizado Webpack para receber arquivos de extensão .JSX

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+         resolve: {
+          extensions: ["*", ".js", ".jsx"]
+        },
         use: {
           loader: "babel-loader",
           query: {
